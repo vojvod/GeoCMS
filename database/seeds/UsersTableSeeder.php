8f30daef76710a97e04242bcf9f0756fa593b38f
vojvod <jaiko@gs39>
2019-02-04 17:25:52 +0200

    20190204

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
+        $user = App\User::create([
           'name' => 'Simeon Taskaris',
           'email' => 'simos.taskaris@gmail.com',
-          'password' => bcrypt('taskaris')
+          'password' => bcrypt('taskaris'),
+          'admin' => 1
+        ]);
+
+        App\Profile::create([
+          'user_id' => $user->id,
+          'avatar' => 'uploads/avatars/1.jpg',
+          'about' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat aliquet pulvinar. Vivamus a molestie dolor.',
+          'facebook' => 'facebook.com',
+          'youtube' => 'youtube.com'
         ]);
     }
 }
